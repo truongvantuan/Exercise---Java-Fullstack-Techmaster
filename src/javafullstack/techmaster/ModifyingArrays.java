2bb4d80b7ac0cd68e85d0732d18c9f6f5a9982c8
truongtuan <truongvantuan@outlook.com.vn>
2020-09-17 17:03:23 +0700

    Add method insertToArray to ModifyingArrays.java

--- src/javafullstack/techmaster/ModifyingArrays.java
+++ src/javafullstack/techmaster/ModifyingArrays.java
@@ -18,12 +18,23 @@ public class ModifyingArrays {
 
         // Add new values to array
         int[] resultArray = new int[inputArray.length + 1];
-        System.out.println("\nNhap gia tri can them vao mang: ");
+        System.out.print("\nNhap gia tri can them vao mang: ");
         int addValue = input.nextInt();
 
         // Call method addtoArray
         resultArray = addToArray(inputArray, addValue);
         System.out.print("Mang sau khi them gia tri " + addValue + " la: ");
+        for (int e : resultArray
+        ) {
+            System.out.print(e + " ");
+        }
+
+        // Call method insertToArray
+        System.out.print("\nNhap gia tri va vi tri can chen vao mang: ");
+        int insertValue = input.nextInt();
+        int position = input.nextInt();
+        resultArray = insertToArray(inputArray, position, insertValue);
+        System.out.println("\nMang sau khi chen gia tri la: ");
         for (int e : resultArray
         ) {
             System.out.print(e + " ");
@@ -39,6 +50,17 @@ public class ModifyingArrays {
         outputArray[inputArray.length] = addValue;
         return outputArray;
     }
+
+    public static int[] insertToArray(int[] inputArray, int position, int insertValue) {
+        int[] outputArray = new int[inputArray.length + 1];
+
+        //Use built-in method System.arraycopy to copy
+        System.arraycopy(inputArray, 0, outputArray, 0, position);
+        System.arraycopy(inputArray, position, outputArray, position + 1, inputArray.length - position);
+        outputArray[position] = insertValue;
+        return outputArray;
+    }
+
 
 }
 
